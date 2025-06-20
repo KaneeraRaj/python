db10f87464ce24e0b2072bb8acf1967ebb5a7a60
KaneeraRaj <shanvi3545@gmail.com>
2025-06-20 19:54:44 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,13 @@
+print("Data type in python")
+a="Kaneera"
+b=15
+c=12.5
+d=True
+print("Name of student",a)
+print("data typeof variable a is",type(a))
+print("Name of student",b)
+print("data typeof variable a is",type(b))
+print("Name of student",c)
+print("data typeof variable a is",type(c))
+print("Name of student",d)
+print("data typeof variable a is",type(d))

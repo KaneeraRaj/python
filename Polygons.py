46e75565604a2292f6142d501dd43a9b1d9bd302
KaneeraRaj <shanvi3545@gmail.com>
2025-06-21 19:41:26 +0530

    Add files via upload

---
+++ Polygons.py
@@ -0,0 +1,24 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+board=turtle.Turtle ()
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+#Square
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+
+board.forward(100)
+board.left(90)

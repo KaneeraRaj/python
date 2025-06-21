8d4b1644239d3d8c9ee736e3e5a4831356bcc2e8
KaneeraRaj <shanvi3545@gmail.com>
2025-06-21 17:19:59 +0530

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,18 @@
+a="Hey Kaneera what's up?"
+b="Hey Pranjala,I am so good so far."
+c="Hey something happened?"
+d="Yes, my cat run into a car."
+e="Oh no! Its okay I will make you laugh."
+f="Hmm..."
+g="What has thirteen hearts but no other organs"
+h="Umm..A deck of card"
+i="Yes, ha... ha..."
+print("Pranjala entered the garden saying:",a)
+print("Kaneera replied:",b)
+print("Pranjala inquired:",c)
+print("Kaneera answered:",d)
+print("Pranjal got sad and said:",e)
+print("Kaneera nodded:",f)
+print("Pranjala asked:",g)
+print("Kaneera said (thinking deeply):",h)
+print("Pranjala said laughingly:",i)

345a55bbc721e0e13b7e217a6b0867aa3d1c20d3
KaneeraRaj <shanvi3545@gmail.com>
2025-06-28 16:20:58 +0530

    Add files via upload

---
+++ after.py
@@ -0,0 +1,23 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+board=turtle.Turtle ()
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+#Rectangle
+board.forward(150)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(150)
+board.left(90)
+
+board.forward(100)
+board.left(90)

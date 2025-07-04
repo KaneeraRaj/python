4af9c8b6d613f54f16ab703a2bc5f2a93b50b9e5
KaneeraRaj <shanvi3545@gmail.com>
2025-07-04 19:37:30 +0530

    Add files via upload

---
+++ absolute value.py
@@ -0,0 +1,8 @@
+def absolutevalue(num):
+ if num>=0:
+       return num
+ else:
+   return-num
+
+print("Absolute value of 89:",absolutevalue(89))
+print("Absolute value of -189:",absolutevalue(-189))

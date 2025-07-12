30219d95c6356d532ba219fde4f6f9c8d32b1491
KaneeraRaj <shanvi3545@gmail.com>
2025-07-12 15:45:28 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,18 @@
+import math
+
+class Circle:
+    def__init__(self,radius):
+    self.radius=radius
+
+def area(self):
+    return math.pi*self.radius*self.radius
+
+def perimeter(self):
+    return 2*math.pi*self.radius
+
+circle1=circle(7)
+
+print( "Radius of circle:",circle1.radius
+     "Area of circle:",circle1.area
+     "Perimeter of circle:", circle1.perimeter)
+        

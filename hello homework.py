2c7f26537d4f0908a4247ffbf543ca84e1269883
KaneeraRaj <shanvi3545@gmail.com>
2025-09-26 19:37:26 +0530

    Add files via upload

---
+++ hello homework.py
@@ -0,0 +1,15 @@
+print("Hello!I am AI bot. Whats your favourite dish?")
+
+dish=input()
+print(f"thats a  good choice!{dish}is super delicious")
+print("whats your hobby(dancing or drawing)")
+hobby=input().lower()
+
+if hobby=="dancing":
+    print("Great!Dancing is the art of moving the body rhythmically, usually to music, to express emotions, ideas, and culture, or simply for enjoyment and exercise. It")
+elif hobby=="drawing":
+    print("Great!A drawing is a visual art form where marks are made on a surface using instruments like pencils, pens, or digital tools to create a picture, design, or object. ")
+else:
+    print("Hmm!You have another great hobbies")
+
+print(f"It was nice chatting with you Goodbye!")

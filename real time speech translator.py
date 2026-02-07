406bf36fc3ee9f9560bce8ed3a79fb44bae7e3db
KaneeraRaj <shanvi3545@gmail.com>
2026-02-07 20:02:30 +0530

    Add files via upload

---
+++ real time speech translator.py
@@ -0,0 +1,88 @@
+import speech_recognition as sr
+import pyttsx3
+from googletrans import Translator
+
+def speak(text,language="en"):
+    engine=pyytx3.init()
+    engine.setProperty('rate',150)
+    voices=engine.getProperty('voices')
+
+
+    if language=="en":
+      engine.setProperty('voice',voices[0].id)
+
+    else:
+      engine.setProperty('voices',voices[1].id)
+
+    engine.say(text)
+    engine.runAndWait
+
+def speech_to_text():
+    recognizer=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("?????Please speak in english...")
+        audio=recognizer.listen(source)
+
+
+    try:
+        print("?????Recognizing speech....")
+        text=recognizer.recognize_google(audio,language="en-Us")
+        print(f"You said :{text}")
+        return text
+
+    except sr.UnknownValueError:
+        print("Could not understand the audio")
+    except sr.RequestError as e:
+        print(f"API error :{e}")
+    return ""
+
+def translate_text(text,target_language="es"):
+    translator=Translator()
+    translation=translator.translate(text,dest=target_language)
+    print(f"?????Translated Text:{translation.text}")
+    return translation.text
+
+def display_language_option():
+    print("???? Available translation languages: ")
+
+print("1. Hindi (hi)")
+
+print("2. Tamil (ta)")
+
+print("3. Telugu (te)")
+
+print("4. Bengali (bn)")
+
+print("5. Marathi (mr)")
+
+print("6. Gujarati (gu)")
+
+print("7. Malayalam (ml)")
+
+print("8. Punjabi (pa)")
+
+
+choice=input("Please select the target language(1-8)")
+
+language_dict={
+    "1":"hi",
+    "2":"ta",
+    "3":"te",
+    "4":"bn",
+    "5":"mr",
+    "6":"gu",
+    "7":"ml",
+    "8":"pa"
+}
+
+def main():
+    target_language=display_language_option()
+    orignal_text=speech_to_text()
+
+
+    if orignal_text:
+        translated_text=translate_text(orignal_text,target_language=target_language)
+        speak(translated_text,language="en")
+        print("Translation spoken out!")
+if __name__=="__main__":
+    main()
